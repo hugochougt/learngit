e5526b63e14195f0e0c735fa280b5554f491c7d2
zhaqiang <hugochougt@gmail.com>
2014-11-11 23:15:49 +0800

    Add config-git.sh file

---
+++ config-git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BaseDir=`dirname $0`
+BaseDir=`cd ${BaseDir}; pwd`
+
+git config --global user.name "zhaqiang"
+git config --global user.email hugochougt@gmail.com
+
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+git config --global color.ui true
+
+ln -s ${BaseDir}/gitignore_global ~/.gitignore_global
